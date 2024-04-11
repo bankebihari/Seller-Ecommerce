4a1cfd64d868f78334b4b70c16ee302d5531577b
bankebihari <bankebihari1206@gmail.com>
2024-04-12 00:03:55 +0530

    adding files

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +91 6200957173</label>
+            <i className='fa fa-envelope'></i>
+            <label> support@seller.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span></span>
+            <label>ENG</label>
+            <span></span>
+            <label>INR</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
---
+++ src/components/discount/Ddata.js
@@ -0,0 +1,48 @@
+const Ddata = [
+  {
+    cover: "./images/discount/discount-1.png",
+    name: "BenuX 2022",
+    price: "Rs 250",
+  },
+  {
+    cover: "./images/discount/discount-2.png",
+    name: "Sony TV 1080p",
+    price: "Rs 450",
+  },
+  {
+    cover: "./images/discount/discount-3.png",
+    name: "Sony PS4",
+    price: "Rs 50",
+  },
+  {
+    cover: "./images/discount/discount-4.png",
+    name: "Setgearr 2022",
+    price: "Rs 100",
+  },
+  {
+    cover: "./images/discount/discount-5.png",
+    name: "Tony BGB",
+    price: "Rs 20",
+  },
+  {
+    cover: "./images/discount/discount-6.png",
+    name: "RG products",
+    price: "Rs 200",
+  },
+  {
+    cover: "./images/discount/discount-7.png",
+    name: "Ranasonic 2022",
+    price: "Rs 300",
+  },
+  {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "Rs 30",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "Rs 80",
+  },
+]
+export default Ddata
